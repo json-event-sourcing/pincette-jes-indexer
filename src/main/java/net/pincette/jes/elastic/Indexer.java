a036ec1966af41e931ff0d0fa4782009013d5995
Werner Donné <werner.donne@pincette.biz>
2020-03-16 16:27:07 +0100

    Strip metadata, remove deleted objects and add logging

--- src/main/java/net/pincette/jes/elastic/Indexer.java
+++ src/main/java/net/pincette/jes/elastic/Indexer.java
@@ -1,24 +1,43 @@
 package net.pincette.jes.elastic;
 
+import static java.lang.String.valueOf;
 import static java.lang.System.exit;
 import static java.util.UUID.randomUUID;
 import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.SEVERE;
+import static java.util.logging.Level.parse;
 import static java.util.logging.Logger.getLogger;
+import static javax.json.Json.createObjectBuilder;
+import static net.pincette.jes.elastic.Logging.log;
 import static net.pincette.jes.util.Configuration.loadDefault;
+import static net.pincette.jes.util.JsonFields.DELETED;
 import static net.pincette.jes.util.JsonFields.ID;
+import static net.pincette.jes.util.JsonFields.TIMESTAMP;
+import static net.pincette.jes.util.JsonFields.TYPE;
+import static net.pincette.jes.util.Kafka.createReliableProducer;
+import static net.pincette.jes.util.Kafka.fromConfig;
 import static net.pincette.jes.util.Streams.start;
 import static net.pincette.json.JsonUtil.string;
+import static net.pincette.util.Util.tryToDoWithRethrow;
+import static net.pincette.util.Util.tryToGetSilent;
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 import com.typesafe.config.Config;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.json.JsonObject;
+import net.pincette.function.SideEffect;
+import net.pincette.jes.util.JsonSerializer;
 import net.pincette.jes.util.Streams;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.BoundRequestBuilder;
+import org.asynchttpclient.Response;
 
 /**
  * This service connects Kafka topics to indices in Elasticsearch. The topics should use the
@@ -28,17 +47,35 @@ import org.asynchttpclient.AsyncHttpClient;
  * @author Werner Donn\u00e9
  */
 public class Indexer {
+  private static final String ENVIRONMENT = "environment";
   private static final String KAFKA = "kafka";
+  private static final String LOG_LEVEL = "logLevel";
+  private static final String LOG_TOPIC = "logTopic";
+  private static final String VERSION = "1.0.1";
   private static final AsyncHttpClient client = asyncHttpClient();
 
   private static void connect(
       final String topic,
       final String uri,
       final String authorizationHeader,
-      final StreamsBuilder builder) {
+      final StreamsBuilder builder,
+      final Logger logger) {
     final KStream<String, JsonObject> stream = builder.stream(topic);
 
-    stream.mapValues(v -> sendMessage(v, createUri(v, uri), authorizationHeader));
+    stream
+        .filter((k, v) -> !v.getBoolean(DELETED, false))
+        .mapValues(
+            v ->
+                sendPutMessage(removeMetadata(v), createUri(v, uri), authorizationHeader)
+                    .thenApply(
+                        response ->
+                            response.getStatusCode() < 400
+                                || SideEffect.<Boolean>run(() -> logError(response, logger))
+                                    .andThenGet(() -> false)));
+
+    stream
+        .filter((k, v) -> v.getBoolean(DELETED, false))
+        .mapValues(v -> sendDeleteMessage(createUri(v, uri), authorizationHeader));
   }
 
   private static String createUri(final JsonObject json, final String uri) {
@@ -50,11 +87,27 @@ public class Indexer {
     return uri + (uri.endsWith("/") ? "" : "/") + index + "/_doc/";
   }
 
+  private static void logError(final Response response, final Logger logger) {
+    logger.log(
+        SEVERE,
+        "{0} {1}\n{2}",
+        new Object[] {
+          valueOf(response.getStatusCode()), response.getStatusText(), response.getResponseBody()
+        });
+  }
+
   public static void main(final String[] args) {
     final StreamsBuilder builder = new StreamsBuilder();
     final Config config = loadDefault();
     final String authorizationHeader = config.getString("elastic.authorizationHeader");
+    final String environment = tryToGetSilent(() -> config.getString(ENVIRONMENT)).orElse("dev");
+    final Level logLevel = parse(tryToGetSilent(() -> config.getString(LOG_LEVEL)).orElse("INFO"));
+    final String logTopic =
+        tryToGetSilent(() -> config.getString(LOG_TOPIC)).orElse("log-" + environment);
+    final Logger logger = getLogger("pincette-jes-indexer");
     final String uri = config.getString("elastic.uri");
+
+    logger.setLevel(logLevel);
 
     config
         .getConfig("indices")
@@ -65,26 +118,48 @@ public class Indexer {
                     e.getKey(),
                     fullUri(uri, e.getValue().unwrapped().toString()),
                     authorizationHeader,
-                    builder));
+                    builder,
+                    logger));
 
-    final Topology topology = builder.build();
+    tryToDoWithRethrow(
+        () ->
+            createReliableProducer(
+                fromConfig(config, KAFKA), new StringSerializer(), new JsonSerializer()),
+        producer -> {
+          final Topology topology = builder.build();
 
-    getLogger("pincette-jes-indexer").log(INFO, "Topology:\n\n {0}", topology.describe());
+          log(logger, logLevel, VERSION, environment, producer, logTopic);
+          logger.log(INFO, "Topology:\n\n {0}", topology.describe());
 
-    if (!start(topology, Streams.fromConfig(config, KAFKA))) {
-      exit(1);
-    }
+          if (!start(topology, Streams.fromConfig(config, KAFKA))) {
+            exit(1);
+          }
+        });
   }
 
-  private static CompletionStage<Boolean> sendMessage(
-      final JsonObject json, final String uri, final String authorizationHeader) {
+  private static JsonObject removeMetadata(final JsonObject message) {
+    return createObjectBuilder(message).remove(ID).remove(TYPE).remove(TIMESTAMP).build();
+  }
+
+  private static BoundRequestBuilder request(
+      final String method, final String uri, final String authorizationHeader) {
     return client
-        .prepare("PUT", uri)
+        .prepare(method, uri)
         .setHeader("Authorization", authorizationHeader)
-        .setHeader("Content-Type", "application/json")
-        .setBody(string(json))
-        .execute()
-        .toCompletableFuture()
-        .thenApply(r -> r.getStatusCode() == 202);
+        .setHeader("Content-Type", "application/json");
+  }
+
+  private static CompletionStage<Response> send(final BoundRequestBuilder request) {
+    return request.execute().toCompletableFuture();
+  }
+
+  private static CompletionStage<Response> sendDeleteMessage(
+      final String uri, final String authorizationHeader) {
+    return send(request("DELETE", uri, authorizationHeader));
+  }
+
+  private static CompletionStage<Response> sendPutMessage(
+      final JsonObject json, final String uri, final String authorizationHeader) {
+    return send(request("PUT", uri, authorizationHeader).setBody(string(json)));
   }
 }
